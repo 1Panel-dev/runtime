0d82fb50c690b9eb6d06a25e910af82a1f09b222
zhengkunwang223 <1paneldev@sina.com>
2026-05-29 15:23:23 +0800

    feat: update hermes

--- hermes-agent/1panel-banner.sh
+++ hermes-agent/1panel-banner.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-set -e
+#!/command/with-contenv sh
+set -eu
 
 printf '\033[1;32m%s\033[0m\n' 'This image is maintained by 1Panel.'
 printf '\033[1;33m%s\033[0m\n' 'For support or issue discussion, please visit:'
 printf '\033[1;36m%s\033[0m\n' 'https://github.com/1Panel-dev/1Panel/discussions'
-
-exec /opt/hermes/docker/entrypoint.sh "$@"
